6b932604401339e047d4fac91192ed49afffb1d9
xiwenge <xiwenge@9ji.com>
2021-05-26 14:59:38 +0800

    feat: 新增了Header组件；style: 给整个todos增加了样式

---
+++ src/components/Header/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="headerContainer">
+    <h1 class="headerText">XWGTodos</h1>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+  },
+  computed: {
+  },
+}
+</script>
+<style lang='scss' scoped>
+.headerContainer {
+  width: 530px;
+  height: 120px;
+  margin: 10px auto;
+  .headerText {
+    font-size: 100px;
+    font-weight: 20px;
+    color: black;
+    margin-top: 100px;
+  }
+}
+</style>
